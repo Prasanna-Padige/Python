75ccd4becc1242a975e0fd4ff491a906f944f0db
Prasanna-Padige <padigeprasanna23@gmail.com>
2026-07-22 23:18:58 +0530

    Calculator

---
+++ project6.py
@@ -0,0 +1,38 @@
+import os
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+operations_dict={
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":divide
+}
+def calculator():
+    number1=int(input("Enter First number:"))
+    for symbol in operations_dict:
+        print(symbol)
+    continue_flag=True
+    while continue_flag:
+        op_symbol=input("Pick one symbol:")
+        number2=int(input("Enter Second number:"))
+        calculator_function=operations_dict[op_symbol]
+        output=calculator_function(number1,number2)
+        print(f"{number1} {op_symbol} {number2} = {output}")
+        should_continue=input(f"Enter 'y' to continue with {output} or 'n' to satrt a new calculation or 'x' to exit")
+        if should_continue=='y':
+            number1=output
+        elif should_continue=='n':
+            continue_flag==False
+            os.system('cls')
+            calculator()
+        else:
+            print("bye")
+            break
+            
+calculator()
